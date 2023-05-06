53eda56a4bb813e22b59621e81afe54b0eeb8f41
Sufyan Mansuri <sufyan8834@gmail.com>
2023-05-06 10:39:48 +0530

    build: :construction_worker: Enable output option

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
