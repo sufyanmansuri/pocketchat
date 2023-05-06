ed85273ab353a4b4905720e19526787576f47946
Sufyan Mansuri <sufyan8834@gmail.com>
2023-05-06 10:55:20 +0530

    build: :construction_worker: Test workflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Login from "@/components/Login";
+import Login from "@/components/Login"
 
 export default function Home() {
   return (
     <>
       <h1>Hello, Next.js</h1>
+      <p>Testing workflow</p>
       <Login />
     </>
-  );
+  )
 }
