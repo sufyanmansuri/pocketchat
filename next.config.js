348fdbc905372d49b6976399f630f3da31442344
Sufyan Mansuri <sufyan8834@gmail.com>
2023-05-06 11:07:05 +0530

    build: :wastebasket: Remove gh workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const isProd = process.env.NODE_ENV === "production"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? "/pocketchat/" : "",
-  images: {
-    unoptimized: true,
-  },
-  output: "export",
   experimental: {
     serverActions: true,
   },
