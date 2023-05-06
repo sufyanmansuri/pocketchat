594474c1127b1107c375af6ce71d3c77a65502c7
Sufyan Mansuri <sufyan8834@gmail.com>
2023-05-06 09:32:26 +0530

    :sparkles: Installed shadcn-ui

---
+++ components/Login.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export default function Login() {
+  async function handleLogin(data: FormData) {
+    "use server";
+    console.log(Object.fromEntries(data.entries()));
+  }
+
+  return (
+    <form action={handleLogin}>
+      <Input name="username" placeholder="Username" />
+      <Input name="password" type="password" placeholder="Password" />
+
+      <Button type="submit">Let&apos;s go!</Button>
+    </form>
+  );
+}
